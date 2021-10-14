1b3af0361d1d01369db7694c4d26e0db8386dce3
huuphat1908 <huuphat1908@gmail.com>
2021-10-14 15:56:17 +0700

    add rating and login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 /* import RMDBLogo from '../../images/react-movie-logo.svg'; */
@@ -7,16 +7,32 @@ import HPMovieLogo from '../../images/HPMovie.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
-const Header = () => (
-    <Wrapper>
-        <Content>
-            <Link to='/'>
-                <LogoImg src={HPMovieLogo} alt='rmdb-logo' />
-            </Link>
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
-        </Content>
-    </Wrapper>
-);
+//context
+import { Context } from '../../context';
+
+const Header = () => {
+    const [user] = useContext(Context);
+    console.log(user);
+
+    return (
+        <Wrapper>
+            <Content>
+                <Link to='/'>
+                    <LogoImg src={HPMovieLogo} alt='rmdb-logo' />
+                </Link>
+                {user ? (
+                    <span>Logged in as: {user.username}</span>
+                ) : (
+                    <Link to='/login'>
+                        <span>Log in</span>
+                    </Link>
+                )
+                }
+                <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+            </Content>
+        </Wrapper>
+    )
+};
 
 export default Header;
 
